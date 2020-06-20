5dcda7fec2f4530522623f9817c1ef6339dc98d0
martinezmatias <matias.sebastian.martinez@gmail.com>
2020-06-20 12:18:20 +0200

    new jar

--- script_runner/autotuning-launch-script/RunSingleDiff.py
+++ script_runner/autotuning-launch-script/RunSingleDiff.py
@@ -1,4 +1,4 @@
 from TaskLauncher import *
 
 
-runProject(out = outResult, path = megadiffpath, subset=1, begin=0, stop = 1, astmodel="JDT", parallel=True)
+runProject(out = outResult, path = megadiffpath, subset=1, begin=0, stop = 1, astmodel="GTSPOON", parallel=True)
