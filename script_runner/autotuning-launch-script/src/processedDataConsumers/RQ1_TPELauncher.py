b90d21f4fba4bc279b1faa7cabbee17ace5c2ef9
martinezmatias <matias.sebastian.martinez@gmail.com>
2020-08-19 17:28:31 +0200

    bugfix k hardcoded

--- script_runner/autotuning-launch-script/src/processedDataConsumers/RQ1_TPELauncher.py
+++ script_runner/autotuning-launch-script/src/processedDataConsumers/RQ1_TPELauncher.py
@@ -8,7 +8,7 @@ def runTPERangeValues(distancespath,datasetname, runTPE, algo, seed):
 	#The first
 	datasetDF = None
 	for i_eval in evals_range:
-		kfold = 2
+		kfold = KFOLD_VALUE
 		for i_ratio in ratioDataset:
 			try:
 				datasetDF = computeHyperOpt(pathResults=distancespath, dfcomplete=datasetDF, kFold=kfold, max_evals=i_eval , algorithm=algo,fractiondata= i_ratio,  dataset = datasetname, runTpe= (runTPE.lower() == "true"), out=RESULTS_PROCESSED_LOCATION, random_seed=seed )
