ab5f11a50e1f95b0ca3b17c3ba0360d5bc412b6c
martinezmatias <matias.sebastian.martinez@gmail.com>
2020-08-21 10:27:06 +0200

    changing even it's deprecated

--- script_runner/autotuning-launch-script/src/execution/deprecated/TPELauncher.py
+++ script_runner/autotuning-launch-script/src/execution/deprecated/TPELauncher.py
@@ -3,7 +3,7 @@ import sys
 from src.processedDataConsumers.EngineHyperOptDAT import *
 from src.commons.Datalocation import *
 
-distancespath = sys.argv[1]
+editscriptsizespath = sys.argv[1]
 
 algorithm = sys.argv[2]
 
@@ -17,4 +17,4 @@ datasetname = sys.argv[6]
 
 runTPE = str(sys.argv[7])
 
-computeHyperOpt(pathResults=distancespath, kFold=kfold, max_evals=i_eval ,fractiondata= i_ratio,  dataset = datasetname, algorithm = algorithm, runTpe= (runTPE.lower() == "true"), out=RESULTS_PROCESSED_LOCATION )
+computeHyperOpt(pathResults=editscriptsizespath, kFold=kfold, max_evals=i_eval ,fractiondata= i_ratio,  dataset = datasetname, algorithm = algorithm, runTpe= (runTPE.lower() == "true"), out=RESULTS_PROCESSED_LOCATION )
