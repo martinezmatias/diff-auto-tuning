72297069c88630f9f68dfb86c0e98cf150711d34
martinezmatias <matias.sebastian.martinez@gmail.com>
2020-08-21 09:05:39 +0200

    visualizer

--- src/main/java/fr/gumtree/autotuning/ResultVisualizer.java
+++ src/main/java/fr/gumtree/autotuning/ResultVisualizer.java
@@ -7,11 +7,17 @@ import java.nio.file.Files;
 import java.util.List;
 
 import org.rendersnake.HtmlCanvas;
+import org.rendersnake.Renderable;
 
+import com.github.difflib.DiffUtils;
+import com.github.difflib.UnifiedDiffUtils;
+import com.github.difflib.algorithm.DiffException;
+import com.github.difflib.patch.Patch;
 import com.github.gumtreediff.actions.ChawatheScriptGenerator;
+import com.github.gumtreediff.client.diff.webdiff.MergelyDiffView2;
+import com.github.gumtreediff.client.diff.webdiff.TextDiffView;
 import com.github.gumtreediff.client.diff.webdiff.VanillaDiffHtmlBuilder;
 import com.github.gumtreediff.client.diff.webdiff.VanillaDiffView;
-import com.github.gumtreediff.gen.jdt.JdtTreeGenerator;
 import com.github.gumtreediff.matchers.CompositeMatchers;
 import com.github.gumtreediff.matchers.CompositeMatchers.ClassicGumtree;
 import com.github.gumtreediff.matchers.CompositeMatchers.CompleteGumtreeMatcher;
@@ -24,14 +30,20 @@ import com.github.gumtreediff.tree.TreeContext;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
+import fr.gumtree.autotuning.treebuilder.JDTTreeBuilder;
+import fr.gumtree.autotuning.treebuilder.SpoonTreeBuilder;
 import fr.gumtree.treediff.jdt.TreeDiffGumTreeBuilder;
 import gumtree.spoon.diff.DiffImpl;
 import gumtree.spoon.diff.operations.Operation;
 
 public class ResultVisualizer {
 
-	public void saveVisualizationRow(String row, String pathMegadiff, String pathOut) throws IOException {
+	public boolean saveVisualizationRow(String row, String pathMegadiff, String pathOut, boolean isJDTModel)
+			throws Exception {
+
+		System.out.println("*****\nRow to analyze " + row);
 		String[] split = row.split(",");
 		String id = split[0];
 		String[] idSplitted = id.split("_");
@@ -49,28 +61,37 @@ public class ResultVisualizer {
 
 		String fl = rootMegadiff.getAbsolutePath() + patch + "_s.java";
 		String fr = rootMegadiff.getAbsolutePath() + patch + "_t.java";
-		System.out.println(fl);
+		// System.out.println(fl);
 
 		File fileLeftt = new File(fl);
 		File fileRight = new File(fr);
 
 		if (!fileLeftt.exists() || !fileRight.exists()) {
 			System.out.println("Error " + fileLeftt.getAbsolutePath() + " does not exist");
-			return;
+			return false;
 		}
 		String lcontent = new String(Files.readAllBytes(fileLeftt.toPath()));
-		ITree tl = new JdtTreeGenerator().generateFrom().string(lcontent).getRoot();
 
 		String rcontent = new String(Files.readAllBytes(fileRight.toPath()));
-		ITree tr = new JdtTreeGenerator().generateFrom().string(rcontent).getRoot();
+
+		ITreeBuilder builder = null;
+		if (isJDTModel) {
+			builder = new JDTTreeBuilder();
+		} else {
+			builder = new SpoonTreeBuilder();
+		}
+		ITree tl = builder.build(fileLeftt);
+		ITree tr = builder.build(fileRight);
 
 		File fout = new File(pathOut);
-		String bestConfig = split[6];
 
+		String bestConfig = split[6];
+		System.out.println("--Running for best: " + bestConfig);
 		GumTreeProperties propertiesBest = createProperties(bestConfig);
 		Matcher matcherBest = createMatcher(bestConfig);
-
+		System.out.println(propertiesBest.getProperties());
 		String nameBest = "best_" + bestConfig;
+
 		saveVisualization(fileLeftt, tl, fileRight, tr, propertiesBest, matcherBest, fout, diffId, nameBest);
 		System.out.println("End best");
 
@@ -78,24 +99,90 @@ public class ResultVisualizer {
 		GumTreeProperties propertiesDefault = createProperties(defaultConfig);
 		Matcher matcherDefault = createMatcher(defaultConfig);
 
+		System.out.println("--Running for default: " + defaultConfig);
+		System.out.println(propertiesDefault.getProperties());
 		String nameDefault = "default_" + defaultConfig;
 		saveVisualization(fileLeftt, tl, fileRight, tr, propertiesDefault, matcherDefault, fout, diffId, nameDefault);
 		System.out.println("End default");
 
+		System.out.println("--Running for nonparameters ");
 		String nameDefaultNoparam = "default_noparam";
 		saveVisualization(fileLeftt, tl, fileRight, tr, new GumTreeProperties(), matcherDefault, fout, diffId,
 				nameDefaultNoparam);
 		System.out.println("End default");
 
-		FileWriter fwleft = new FileWriter(
-				new File(fout.getAbsoluteFile() + File.separator + diffId + File.separator + filename + "_left.java"));
+		File fileLoriginal = new File(
+				fout.getAbsoluteFile() + File.separator + diffId + File.separator + filename + "_left.java");
+		FileWriter fwleft = new FileWriter(fileLoriginal);
 		fwleft.write(lcontent);
 		fwleft.close();
 
-		FileWriter fwright = new FileWriter(
-				new File(fout.getAbsoluteFile() + File.separator + diffId + File.separator + filename + "_right.java"));
+		File fileRoriginal = new File(
+				fout.getAbsoluteFile() + File.separator + diffId + File.separator + filename + "_right.java");
+		FileWriter fwright = new FileWriter(fileRoriginal);
 		fwright.write(rcontent);
 		fwright.close();
+
+		///
+		// exportMergely(diffId, fout, fileLoriginal, fileRoriginal);
+
+		exportUnifiedDiff(diffId, fout, fileLoriginal, fileRoriginal);
+
+		return true;
+	}
+
+	public void exportUnifiedDiff(String diffId, File fout, File fileLoriginal, File fileRoriginal)
+			throws IOException, DiffException {
+		// build simple lists of the lines of the two testfiles
+		List<String> original = Files.readAllLines(fileLoriginal.toPath());
+		List<String> revised = Files.readAllLines(fileRoriginal.toPath());
+
+		// compute the patch: this is the diffutils part
+		Patch<String> patch2 = DiffUtils.diff(original, revised);
+
+		// generating unified diff format
+		List<String> unifiedDiff = UnifiedDiffUtils.generateUnifiedDiff(fileLoriginal.getName(),
+				fileRoriginal.getName(), original, patch2, 3);
+
+		File parentDiff = new File(fout.getAbsoluteFile() + File.separator + diffId + File.separator);
+
+		File diffFile = new File(parentDiff.getAbsolutePath() + File.separator + "diff.txt");
+
+		parentDiff.mkdirs();
+
+		FileWriter fw = new FileWriter(diffFile);
+		for (String line : unifiedDiff) {
+			fw.write(line);
+			fw.write("\n");
+		}
+		fw.close();
+		System.out.println("save at " + diffFile.getAbsolutePath());
+
+		// System.out.println(patch2);
+
+		// simple output the computed patch to console
+		// for (AbstractDelta<String> delta : patch2.getDeltas()) {
+		// System.out.println(delta);
+		// }
+	}
+
+	public void exportMergely(String diffId, File fout, File fileLoriginal, File fileRoriginal) throws IOException {
+		Renderable view2 = new MergelyDiffView2(0, fileLoriginal.getName(), fileRoriginal.getName());
+		HtmlCanvas html = new HtmlCanvas();
+		view2.renderOn(html);
+
+		String html2 = html.toHtml();
+		html2 = html2.replace("/dist/", "../../libs/dist/");
+
+		File parentDiff = new File(fout.getAbsoluteFile() + File.separator + diffId + File.separator);
+
+		File htmlFile = new File(parentDiff.getAbsolutePath() + File.separator + "simple.html");
+		parentDiff.mkdirs();
+
+		FileWriter fw = new FileWriter(htmlFile);
+		fw.write(html2);
+		fw.close();
+		System.out.println("save at " + htmlFile.getAbsolutePath());
 	}
 
 	private Matcher createMatcher(String bestConfig) {
@@ -130,33 +217,59 @@ public class ResultVisualizer {
 
 		List<Operation> actionsAll = result.getAllOperations();
 
-		System.out.println("Size  " + actionsAll.size());
-		System.out.println("All " + actionsAll);
+		File parentDiff = new File(fout.getAbsoluteFile() + File.separator + diffId + File.separator);
+		parentDiff.mkdirs();
 
-		System.out.println("Visualization:");
-		com.github.gumtreediff.actions.Diff diffgtt = result.getNativeDiff();
+		com.github.gumtreediff.actions.Diff diffgtt = saveDiffInfo(parentDiff, result, actionsAll, configId);
 
 		VanillaDiffHtmlBuilder builder = new VanillaDiffHtmlBuilder(fileLeftt, fileRight,
 				(com.github.gumtreediff.actions.Diff) diffgtt);
 		builder.produce();
 
-		VanillaDiffView view = new VanillaDiffView(fileLeftt, fileRight, (com.github.gumtreediff.actions.Diff) diffgtt);
-		HtmlCanvas html = new HtmlCanvas();
-		view.renderOn(html);
+		//
 
-		String html2 = html.toHtml();
-		html2 = html2.replace("/dist/vanilla.css", "../dist/vanilla.css");
+		// VanillaDiffView
+		Renderable view = new VanillaDiffView(fileLeftt, fileRight, (com.github.gumtreediff.actions.Diff) diffgtt);
+		saveRenderable(parentDiff, diffId, configId, view, "vanillaDiffView");
 
-		File parentDiff = new File(fout.getAbsoluteFile() + File.separator + diffId + File.separator);
+		// Text
+		Renderable viewText = new TextDiffView(fileLeftt, fileRight, (com.github.gumtreediff.actions.Diff) diffgtt);
+		saveRenderable(parentDiff, diffId, configId, viewText, "textDiffView");
 
-		File htmlFile = new File(parentDiff.getAbsolutePath() + File.separator + "visualization_" + configId + ".html");
-		parentDiff.mkdirs();
+		// Unified
+		saveUnified(configId, diffgtt, parentDiff);
 
-		FileWriter fw = new FileWriter(htmlFile);
-		fw.write(html2);
+	}
+
+	public com.github.gumtreediff.actions.Diff saveDiffInfo(File parentDiff, DiffImpl result,
+			List<Operation> actionsAll, String configId) throws IOException {
+		System.out.println("Size root ops " + result.getRootOperations().size());
+		System.out.println("Size all ops " + actionsAll.size());
+
+		// System.out.println("All " + actionsAll);
+
+		com.github.gumtreediff.actions.Diff diffgtt = result.getNativeDiff();
+
+		System.out.println("Native: Size all ops " + diffgtt.editScript.asList().size());
+
+		JsonObject jsonunif = new JsonObject();
+		jsonunif.addProperty("nrAllActions", actionsAll.size());
+		jsonunif.addProperty("nrRootActions", result.getRootOperations().size());
+
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+		String json = gson.toJson(jsonunif);
+
+		File uniflFile = new File(parentDiff.getAbsolutePath() + File.separator + "infoActions_" + configId + ".json");
+		FileWriter fw = new FileWriter(uniflFile);
+		fw.write(json);
 		fw.close();
-		System.out.println("save at " + htmlFile.getAbsolutePath());
 
+		return diffgtt;
+	}
+
+	public void saveUnified(String configId, com.github.gumtreediff.actions.Diff diffgtt, File parentDiff)
+			throws IOException {
 		TreeDiffGumTreeBuilder unifiedrep = new TreeDiffGumTreeBuilder();
 		JsonElement jsonunif = unifiedrep.build(diffgtt);
 
@@ -164,10 +277,29 @@ public class ResultVisualizer {
 
 		String json = gson.toJson(jsonunif);
 
-		File uniflFile = new File(parentDiff.getAbsolutePath() + File.separator + "unified_" + configId + ".json");
-		fw = new FileWriter(uniflFile);
+		File uniflFile = new File(
+				parentDiff.getAbsolutePath() + File.separator + "treeDiffSerialFormat_" + configId + ".json");
+		FileWriter fw = new FileWriter(uniflFile);
 		fw.write(json);
 		fw.close();
+	}
 
+	public File saveRenderable(File parentDiff, String diffId, String configId, Renderable view, String name)
+			throws IOException {
+		HtmlCanvas html = new HtmlCanvas();
+		view.renderOn(html);
+
+		String html2 = html.toHtml();
+		// html2 = html2.replace("/dist/vanilla.css", "../dist/vanilla.css");
+		html2 = html2.replace("/dist/", "../../libs/dist/");
+		// html2 = html2.replace("/monaco/", "../monaco/");
+
+		File htmlFile = new File(parentDiff.getAbsolutePath() + File.separator + name + "_" + configId + ".html");
+
+		FileWriter fw = new FileWriter(htmlFile);
+		fw.write(html2);
+		fw.close();
+		System.out.println("save at " + htmlFile.getAbsolutePath());
+		return parentDiff;
 	}
 }
