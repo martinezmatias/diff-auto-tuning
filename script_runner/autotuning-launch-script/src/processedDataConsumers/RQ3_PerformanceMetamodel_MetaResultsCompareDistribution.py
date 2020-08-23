f12a3495bb61f6a8fd8405e2004727f38362fd8b
martinezmatias <matias.sebastian.martinez@gmail.com>
2020-08-23 08:49:36 +0200

    comparison distr

--- script_runner/autotuning-launch-script/src/processedDataConsumers/RQ3_PerformanceMetamodel_MetaResultsCompareDistribution.py
+++ script_runner/autotuning-launch-script/src/processedDataConsumers/RQ3_PerformanceMetamodel_MetaResultsCompareDistribution.py
@@ -7,6 +7,7 @@ from src.commons.CohenEffectSize import *
 from scipy.stats import wilcoxon, kruskal
 from src.commons.Datalocation import *
 from src.commons.DiffAlgorithmMetadata import *
+import pingouin as pg
 def compareDistributions(pathResults ="{}/distance_per_diff_GTSPOON.csv".format(RESULTS_PROCESSED_LOCATION), keyBestConfiguration= defaultConfigurations["ClassicGumtree"], keyDefaultConfiguration="ClassicGumtree_0.5_1000_2"):
 	df = pandas.read_csv(pathResults, sep=",")
 
@@ -21,6 +22,7 @@ def compareDistributions(pathResults ="{}/distance_per_diff_GTSPOON.csv".format(
 def crossResults(pathDistances, pathSize, keyBestConfiguration, keyDefaultConfiguration):
 	print("reading data for {}".format(pathDistances))
 	dfDistances = pandas.read_csv(pathDistances, sep=",")
+	print("reading data for {}".format(pathSize))
 	dfSize = pandas.read_csv(pathSize, sep=",")
 	return crossResultsDatasets(dfDistances, dfSize,  keyBestConfiguration, keyDefaultConfiguration)
 
@@ -32,15 +34,15 @@ def crossResultsDatasets(dfDistances, dfSize, keyBestConfiguration, keyDefaultCo
 	print("Nr columns In Distances {}".format(len(columnsDistances)))
 	diffsDistances = dfDistances['diff']
 	allDiffDistances = list(diffsDistances.values)
-	print("nr of diffs Distances {}".format(len(allDiffDistances)))
+	#print("nr of diffs Distances {}".format(len(allDiffDistances)))
 
 	##now ed size
 	columnsSize = list(dfSize.columns)
-	print("Nr columns In Sizes {}".format(len(columnsSize)))
+	#print("Nr columns In Sizes {}".format(len(columnsSize)))
 
 	diffsSize = dfSize['diff']
 	allDiffSizes = list(diffsSize.values)
-	print("nr of diffs Sizes {}".format(len(allDiffSizes)))
+	#print("nr of diffs Sizes {}".format(len(allDiffSizes)))
 
 	print("Comparing Best {} and Default {} ".format(keyBestConfiguration, keyDefaultConfiguration))
 
@@ -50,17 +52,20 @@ def crossResultsDatasets(dfDistances, dfSize, keyBestConfiguration, keyDefaultCo
 	valuesSizeBestConfiguration = list(dfSize[keyBestConfiguration].values)
 	valuesSizeDefaultConfiguration = list(dfSize[keyDefaultConfiguration].values)
 
-	print("nr of valuesBestConfiguration {}".format(len(valuesDistancesBestConfiguration)))
-	print("nr of valuesDefaultConfiguration {}".format(len(valuesDistancesDefaultConfiguration)))
-	xBest = []
-	xDefault = []
+	#print("nr of valuesBestConfiguration {}".format(len(valuesDistancesBestConfiguration)))
+	#print("nr of valuesDefaultConfiguration {}".format(len(valuesDistancesDefaultConfiguration)))
+
 	nrNAN = 0
 	nrNotNAN = 0
 	bothBest = 0
 	onlyBestIsBest = 0
 	onlyDefaultIsBest = 0
 	noneIsBest = 0
+	differencesFavorBest = []
+	differencesFavorDefault = []
 
+	sizesBest = []
+	sizeDefault = []
 	for i in range(0, len(allDiffDistances)):
 
 		iDiffD = allDiffDistances[i]
@@ -74,14 +79,23 @@ def crossResultsDatasets(dfDistances, dfSize, keyBestConfiguration, keyDefaultCo
 
 		iBest =  valuesDistancesBestConfiguration[i]
 		iDefault = valuesDistancesDefaultConfiguration[i]
+
 		if math.isnan(iBest) and  math.isnan(iDefault):
 			nrNAN+=1
 		else:
 			nrNotNAN+=1
 
+			if not np.isnan(valuesSizeDefaultConfiguration[i]) : #only commented to R
+			#if not np.isnan(valuesSizeDefaultConfiguration[i]) and not np.isnan(valuesSizeBestConfiguration[i]):
+				sizeDefault.append(int(valuesSizeDefaultConfiguration[i]))
+
+			if not np.isnan(valuesSizeBestConfiguration[i]) :
+			#if not np.isnan(valuesSizeDefaultConfiguration[i]) and not np.isnan(valuesSizeBestConfiguration[i]):
+				sizesBest.append(int(valuesSizeBestConfiguration[i]))
+
 			if iBest == 0 and iDefault == 0:
 				bothBest+=1
-				if valuesSizeBestConfiguration[i] != valuesSizeBestConfiguration[i]:
+				if valuesSizeBestConfiguration[i] != valuesSizeDefaultConfiguration[i]:
 					print("Error: values must match")
 					return
 
@@ -91,32 +105,66 @@ def crossResultsDatasets(dfDistances, dfSize, keyBestConfiguration, keyDefaultCo
 			elif iBest == 0:
 				onlyBestIsBest+=1
 				#print("{} Best better Size: {} {} {} ".format(i,iDiffD, valuesSizeBestConfiguration[i], valuesSizeDefaultConfiguration[i]))
+				if not np.isnan(valuesSizeDefaultConfiguration[i]) and not np.isnan(valuesSizeBestConfiguration[i]) :
+					differencesFavorBest.append(float(valuesSizeDefaultConfiguration[i]) - float(valuesSizeBestConfiguration[i]))
+
 			elif iDefault == 0:
 				onlyDefaultIsBest+=1
-				#print("{} Best default Size: {} {} {} ".format(i, iDiffD, valuesSizeDefaultConfiguration[i], valuesSizeBestConfiguration[i]))
+				if not np.isnan(valuesSizeDefaultConfiguration[i]) and not np.isnan(valuesSizeBestConfiguration[i]) :
+					differencesFavorDefault.append(float(valuesSizeBestConfiguration[i]) - float(valuesSizeDefaultConfiguration[i]))
 			else:
 				noneIsBest+=1
 
-	percentageBest = (onlyBestIsBest + bothBest) / nrNotNAN
-	percentageDefault = (onlyDefaultIsBest + bothBest) / nrNotNAN
-	print("nan {}, non nan {}, both best {} ({:.5f}), only best {} ({:.5f}), only default {} ({:.5f}), noOneIsTheBest {}".format(nrNAN, nrNotNAN, bothBest,
+	percentageBest = (onlyBestIsBest) / nrNotNAN
+	percentageDefault = (onlyDefaultIsBest ) / nrNotNAN
+	print("nan {}, non nan {}, both best {} ({:.5f}), only best {} ({:.5f}), only default {} ({:.5f}), noOneIsTheBest {} ({:.5f})".format(nrNAN, nrNotNAN, bothBest,
 																																 bothBest / nrNotNAN,
 																																 onlyBestIsBest,
 																																 percentageBest,
 																																 onlyDefaultIsBest,
 																																 percentageDefault,
-																																 noneIsBest))
-
-	#for i in range(0, len(valuesDefaultConfiguration)):
-	#	if not np.isnan(valuesBestConfiguration[i]) and not np.isnan(valuesDefaultConfiguration[i]):
-	#		xBest.append(valuesBestConfiguration[i])
-	#		xDefault.append(valuesDefaultConfiguration[i])
-
-	print("Size best {} size default {}".format(len(xBest), len(xDefault)))
+																																 noneIsBest, noneIsBest/nrNotNAN))
 
 
+	print("best distance favor {}  ".format(len(differencesFavorBest)))
+	print("default distance favor  {}  ".format(len(differencesFavorDefault)))
 	#saveFile("{}/paired_values_best_{}_1.csv".format(RESULTS_ROW_LOCATION,keyBestConfiguration), xBest)
 	#saveFile("{}/paired_values_default_{}_2.csv".format(RESULTS_ROW_LOCATION,keyDefaultConfiguration), xDefault)
+
+	print("avg favor best {:.5f} avg favor default {:.5f}".format(np.mean(differencesFavorBest), np.mean(differencesFavorDefault)) )
+
+	if False:
+		print("best sizes {}  ".format(len(sizesBest)))
+		print("best default {}  ".format(len(sizeDefault)))
+
+		#https://matplotlib.org/3.1.1/gallery/statistics/histogram_multihist.html
+		#plt.hist([sizesBest, sizeDefault], alpha=0.5, bins=10000, label=["Best", "Default"]) #histtype = 'step', stacked = True, fill = True,
+		plt.hist([sizesBest, sizeDefault], alpha=0.5,bins=5000,histtype = 'step', label=["Best", "Default"])
+		plt.legend(["Best", "Default"])
+
+		plt.xlim(0, 50)
+		plt.show()
+
+		stat, p = scipy.stats.mannwhitneyu(sizesBest, sizeDefault,alternative='two-sided')
+
+		print('sizes  mannwhitneyu stat=%.3f, p=%.3f' % (stat, p))
+
+		stat, p = scipy.stats.mannwhitneyu(differencesFavorBest, differencesFavorDefault, alternative='two-sided')
+
+		print('differences  mannwhitneyu stat=%.3f, p=%.3f' % (stat, p))
+
+		saveFile("{}/paired_values_best_{}_1.csv".format(RESULTS_ROW_LOCATION,keyBestConfiguration), sizesBest)
+		saveFile("{}/paired_values_default_{}_2.csv".format(RESULTS_ROW_LOCATION,keyDefaultConfiguration), sizeDefault)
+
+		plt.boxplot([differencesFavorBest, differencesFavorDefault], showfliers=True)
+		plt.legend(["Best", "Default"])
+		plt.show()
+
+		plt.hist([differencesFavorBest, differencesFavorDefault], alpha=0.5, bins=5000, histtype='step', label=["Best", "Default"])
+		plt.legend(["Best", "Default"])
+		plt.xlim(0, 100)
+		plt.show()
+
 	print("END-ok")
 	return percentageBest, percentageDefault
 
@@ -147,13 +195,25 @@ def compareDistribution(df, keyBestConfiguration, keyDefaultConfiguration):
 
 		plt.show()
 
+	print("First avg {} median {} std {} ".format(np.mean(xBest), np.median(xBest), np.std(xBest)))
+	print("Second avg {} median {} std {} ".format(np.mean(xDefault), np.median(xDefault), np.std(xDefault)))
+
+	fig, ax = plt.subplots()
+	#plt.kde(ax=[xBest,xDefault], legend=False, title='Histogram: A vs. B')
+	plt.hist(xBest, alpha=0.5)
+	plt.hist(xDefault, alpha=0.5)
+	plt.xlim(0, 60)
+	plt.show()
+
 	#ax = sns.violinplot(data=[xBest,xDefault], split=True, orient="v", inner="quartile", cut=0, showfliers = False )
 	#ax.set_xticklabels(['first', 'second'])
 	plt.boxplot([xBest,xDefault], showfliers=False)
 	plt.show()
 
+	if True:
+		return
 
-	stat, p =	scipy.stats.mannwhitneyu(xBest, xDefault)
+	stat, p =	scipy.stats.mannwhitneyu(xBest, xDefault, alternative='two-sided')
 
 	print(' mannwhitneyu stat=%.3f, p=%.3f' % (stat, p))
 
@@ -180,12 +240,12 @@ def compareDistribution(df, keyBestConfiguration, keyDefaultConfiguration):
 	print("Kendall's tau {} ".format(scipy.stats.kendalltau(xDefault, xBest)))  # Pearson's r
 
 	# https://pingouin-stats.org/index.html
-	import pingouin as pg
+
 	print("normality best: {}".format(pg.normality(xBest)))
 	print("normality default: {}".format(pg.normality(xDefault)))
 	#print(pg.multivariate_normality(xBest))
 	#https://pingouin-stats.org/api.html#effect-sizes
-	print("eff size % f" % pg.compute_effsize(xBest, xDefault))
+	#print("eff size % f" % pg.compute_effsize(xBest, xDefault))
 	#https://pingouin-stats.org/generated/pingouin.mwu.html#pingouin.mwu
 	stats = pg.mwu(xBest, xDefault, tail='two-sided')
 	print("pingouin MWU:\n {}".format(stats))
@@ -195,8 +255,8 @@ def compareDistribution(df, keyBestConfiguration, keyDefaultConfiguration):
 
 	#https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.kruskal.html
 
-	saveFile("{}/paired_values_best_{}_1.csv".format(RESULTS_ROW_LOCATION,keyBestConfiguration), xBest)
-	saveFile("{}/paired_values_default_{}_2.csv".format(RESULTS_ROW_LOCATION,keyDefaultConfiguration), xDefault)
+	#saveFile("{}/paired_values_best_{}_1.csv".format(RESULTS_ROW_LOCATION,keyBestConfiguration), xBest)
+	#saveFile("{}/paired_values_default_{}_2.csv".format(RESULTS_ROW_LOCATION,keyDefaultConfiguration), xDefault)
 
 def saveFile(filename, pvalues1):
 	fout1 = open(filename, 'w')
