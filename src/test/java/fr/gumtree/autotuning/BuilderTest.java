c8b740f34227c15918168657021abffe6fb8746f
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-02-01 13:47:02 +0100

    updating spoon version + test

---
+++ src/test/java/fr/gumtree/autotuning/BuilderTest.java
@@ -0,0 +1,233 @@
+package fr.gumtree.autotuning;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.github.gumtreediff.actions.ChawatheScriptGenerator;
+import com.github.gumtreediff.actions.SimplifiedChawatheScriptGenerator;
+import com.github.gumtreediff.actions.model.Action;
+import com.github.gumtreediff.matchers.CompositeMatchers;
+import com.github.gumtreediff.matchers.GumTreeProperties;
+import com.github.gumtreediff.tree.Tree;
+
+import fr.gumtree.autotuning.treebuilder.JDTTreeBuilder;
+import fr.gumtree.autotuning.treebuilder.SpoonTreeBuilder;
+import spoon.reflect.code.CtReturn;
+import spoon.reflect.declaration.CtAnnotation;
+
+public class BuilderTest {
+
+	final File rootMegadiff = new File("./examples/");
+
+	@Test
+	public void testChangesSpoon_1_02f3fd() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		TuningEngine engine = new TuningEngine();
+
+		String fl = rootMegadiff.getAbsolutePath()
+				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_s.java";
+		String fr = rootMegadiff.getAbsolutePath()
+				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_t.java";
+
+		Tree tl = null;
+		Tree tr = null;
+		SpoonTreeBuilder builder = new SpoonTreeBuilder();
+		tl = builder.build(new File(fl));
+		tr = builder.build(new File(fr));
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		ChawatheScriptGenerator edGenerator = new ChawatheScriptGenerator();
+
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+		System.out.println(actionsAll);
+		assertEquals(1, actionsAll.size());
+	}
+
+	@Test
+	public void testChangesJDT_1_02f3fd() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		TuningEngine engine = new TuningEngine();
+
+		String fl = rootMegadiff.getAbsolutePath()
+				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_s.java";
+		String fr = rootMegadiff.getAbsolutePath()
+				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_t.java";
+
+		Tree tl = null;
+		Tree tr = null;
+		JDTTreeBuilder builder = new JDTTreeBuilder();
+		tl = builder.build(new File(fl));
+		tr = builder.build(new File(fr));
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		SimplifiedChawatheScriptGenerator edGenerator = new SimplifiedChawatheScriptGenerator();
+
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+
+		System.out.println(actionsAll);
+		assertEquals(1, actionsAll.size());
+	}
+
+	@Test
+	public void testChangesJDT_1_0a664d() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		TuningEngine engine = new TuningEngine();
+
+		String fl = rootMegadiff.getAbsolutePath()
+				+ "/1_0a664d752c4b0e5a7fb6f06d005181a0c9dc2905/FlowControlService/1_0a664d752c4b0e5a7fb6f06d005181a0c9dc2905_FlowControlService_s.java";
+		String fr = rootMegadiff.getAbsolutePath()
+				+ "/1_0a664d752c4b0e5a7fb6f06d005181a0c9dc2905/FlowControlService/1_0a664d752c4b0e5a7fb6f06d005181a0c9dc2905_FlowControlService_t.java";
+
+		Tree tl = null;
+		Tree tr = null;
+		JDTTreeBuilder builder = new JDTTreeBuilder();
+		tl = builder.build(new File(fl));
+		tr = builder.build(new File(fr));
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		SimplifiedChawatheScriptGenerator edGenerator = new SimplifiedChawatheScriptGenerator();
+
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+
+		System.out.println(actionsAll);
+		assertEquals(7, actionsAll.size());
+	}
+
+	@Test
+	public void testChangesSpoon_1_0a664d() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		TuningEngine engine = new TuningEngine();
+
+		String fl = rootMegadiff.getAbsolutePath()
+				+ "/1_0a664d752c4b0e5a7fb6f06d005181a0c9dc2905/FlowControlService/1_0a664d752c4b0e5a7fb6f06d005181a0c9dc2905_FlowControlService_s.java";
+		String fr = rootMegadiff.getAbsolutePath()
+				+ "/1_0a664d752c4b0e5a7fb6f06d005181a0c9dc2905/FlowControlService/1_0a664d752c4b0e5a7fb6f06d005181a0c9dc2905_FlowControlService_t.java";
+
+		Tree tl = null;
+		Tree tr = null;
+		SpoonTreeBuilder builder = new SpoonTreeBuilder();
+
+		tl = builder.build(new File(fl));
+		tr = builder.build(new File(fr));
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		SimplifiedChawatheScriptGenerator edGenerator = new SimplifiedChawatheScriptGenerator();
+
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+
+		System.out.println(actionsAll);
+		assertEquals(1, actionsAll.size());
+
+		assertTrue(actionsAll.get(0).getNode().getMetadata("spoon_object") instanceof CtReturn);
+
+	}
+
+	@Test
+	public void testChangesSpoon_3_04f0e8() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		TuningEngine engine = new TuningEngine();
+
+		String fl = rootMegadiff.getAbsolutePath()
+				+ "/3_04f0e8f7a3545cf877c10967396b06595d57c34a/JavaExtensions/3_04f0e8f7a3545cf877c10967396b06595d57c34a_JavaExtensions_s.java";
+		String fr = rootMegadiff.getAbsolutePath()
+				+ "/3_04f0e8f7a3545cf877c10967396b06595d57c34a/JavaExtensions/3_04f0e8f7a3545cf877c10967396b06595d57c34a_JavaExtensions_t.java";
+
+		Tree tl = null;
+		Tree tr = null;
+		SpoonTreeBuilder builder = new SpoonTreeBuilder();
+
+		tl = builder.build(new File(fl));
+		tr = builder.build(new File(fr));
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		SimplifiedChawatheScriptGenerator edGenerator = new SimplifiedChawatheScriptGenerator();
+
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+
+		System.out.println(actionsAll);
+		assertEquals(4, actionsAll.size());
+
+		assertTrue(actionsAll.get(0).getNode().getMetadata("spoon_object") instanceof CtReturn);
+
+	}
+
+	@Test
+	public void testChangesJDT_1_02f3() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		TuningEngine engine = new TuningEngine();
+
+		String fl = rootMegadiff.getAbsolutePath()
+				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_s.java";
+		String fr = rootMegadiff.getAbsolutePath()
+				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_t.java";
+
+		Tree tl = null;
+		Tree tr = null;
+		JDTTreeBuilder builder = new JDTTreeBuilder();
+
+		tl = builder.build(new File(fl));
+		tr = builder.build(new File(fr));
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		SimplifiedChawatheScriptGenerator edGenerator = new SimplifiedChawatheScriptGenerator();
+
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+
+		System.out.println(actionsAll);
+		assertEquals(1, actionsAll.size());
+
+		assertTrue(actionsAll.get(0).getNode().getType().name.equals("MarkerAnnotation"));
+
+	}
+
+	@Test
+	public void testChangesSpoon_1_02f3() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		TuningEngine engine = new TuningEngine();
+
+		String fl = rootMegadiff.getAbsolutePath()
+				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_s.java";
+		String fr = rootMegadiff.getAbsolutePath()
+				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_t.java";
+
+		Tree tl = null;
+		Tree tr = null;
+		SpoonTreeBuilder builder = new SpoonTreeBuilder();
+
+		tl = builder.build(new File(fl));
+		tr = builder.build(new File(fr));
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		SimplifiedChawatheScriptGenerator edGenerator = new SimplifiedChawatheScriptGenerator();
+
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+
+		System.out.println(actionsAll);
+		assertEquals(1, actionsAll.size());
+
+		assertTrue(actionsAll.get(0).getNode().getMetadata("spoon_object") instanceof CtAnnotation);
+
+	}
+
+//
+
+}
--- src/test/java/fr/gumtree/autotuning/CasesTest.java
+++ src/test/java/fr/gumtree/autotuning/CasesTest.java
@@ -10,22 +10,77 @@ import java.util.List;
 import org.junit.Test;
 
 import com.github.gumtreediff.actions.ChawatheScriptGenerator;
+import com.github.gumtreediff.actions.EditScriptGenerator;
+import com.github.gumtreediff.actions.SimplifiedChawatheScriptGenerator;
 import com.github.gumtreediff.actions.model.Action;
 import com.github.gumtreediff.gen.jdt.JdtTreeGenerator;
 import com.github.gumtreediff.matchers.CompositeMatchers;
 import com.github.gumtreediff.matchers.ConfigurationOptions;
 import com.github.gumtreediff.matchers.GumTreeProperties;
-import com.github.gumtreediff.tree.ITree;
+import com.github.gumtreediff.tree.Tree;
 
-import gumtree.spoon.AstComparator;
-import gumtree.spoon.builder.SpoonGumTreeBuilder;
+import fr.gumtree.autotuning.treebuilder.JDTTreeBuilder;
+import fr.gumtree.autotuning.treebuilder.SpoonTreeBuilder;
 
-public class DiffTest {
+public class CasesTest {
 
-	private SpoonGumTreeBuilder scanner = new SpoonGumTreeBuilder();
-	private AstComparator diff = new AstComparator();
+//	private SpoonGumTreeBuilder scanner = new SpoonGumTreeBuilder();
+	// private AstComparator diff = new AstComparator();
 
 	final File rootMegadiff = new File("./examples/");
+
+	@Test
+	public void testChangesSpoon() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		TuningEngine engine = new TuningEngine();
+
+		String fl = rootMegadiff.getAbsolutePath()
+				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_s.java";
+		String fr = rootMegadiff.getAbsolutePath()
+				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_t.java";
+
+		Tree tl = null;
+		Tree tr = null;
+		SpoonTreeBuilder builder = new SpoonTreeBuilder();
+		tl = builder.build(new File(fl));
+		tr = builder.build(new File(fr));
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		ChawatheScriptGenerator edGenerator = new ChawatheScriptGenerator();
+
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+		System.out.println(actionsAll);
+		assertEquals(1, actionsAll.size());
+	}
+
+	@Test
+	public void testChangesJDT() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		TuningEngine engine = new TuningEngine();
+
+		String fl = rootMegadiff.getAbsolutePath()
+				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_s.java";
+		String fr = rootMegadiff.getAbsolutePath()
+				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_t.java";
+
+		Tree tl = null;
+		Tree tr = null;
+		JDTTreeBuilder builder = new JDTTreeBuilder();
+		tl = builder.build(new File(fl));
+		tr = builder.build(new File(fr));
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		SimplifiedChawatheScriptGenerator edGenerator = new SimplifiedChawatheScriptGenerator();
+
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+
+		System.out.println(actionsAll);
+		assertEquals(1, actionsAll.size());
+	}
 
 	@Test
 	public void testChangeInAnnotationSpoonGranularity() throws Exception {
@@ -39,8 +94,8 @@ public class DiffTest {
 		String fr = rootMegadiff.getAbsolutePath()
 				+ "/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e/Version/1_02f3fd442349d4e7fdfc9c31a82bb1638db8495e_Version_t.java";
 
-		ITree tl = null;
-		ITree tr = null;
+		Tree tl = null;
+		Tree tr = null;
 
 		tl = scanner.getTree(diff.getCtType(new File(fl)));
 		tr = scanner.getTree(diff.getCtType(new File(fr)));
@@ -119,8 +174,8 @@ public class DiffTest {
 		String fr = rootMegadiff.getAbsolutePath()
 				+ "/1_0a664d752c4b0e5a7fb6f06d005181a0c9dc2905/FlowControlService/1_0a664d752c4b0e5a7fb6f06d005181a0c9dc2905_FlowControlService_t.java";
 		System.out.println(fl);
-		ITree tl = null;
-		ITree tr = null;
+		Tree tl = null;
+		Tree tr = null;
 
 		tl = scanner.getTree(diff.getCtType(new File(fl)));
 		tr = scanner.getTree(diff.getCtType(new File(fr)));
@@ -152,10 +207,10 @@ public class DiffTest {
 		System.out.println(fl);
 
 		String lc = new String(Files.readAllBytes(new File(fl).toPath()));
-		ITree tl = new JdtTreeGenerator().generateFrom().string(lc).getRoot();
+		Tree tl = new JdtTreeGenerator().generateFrom().string(lc).getRoot();
 
 		lc = new String(Files.readAllBytes(new File(fr).toPath()));
-		ITree tr = new JdtTreeGenerator().generateFrom().string(lc).getRoot();
+		Tree tr = new JdtTreeGenerator().generateFrom().string(lc).getRoot();
 
 		TuningEngine engine = new TuningEngine();
 
@@ -240,6 +295,237 @@ public class DiffTest {
 		System.out.println(actionsAll);
 
 		assertEquals(7, actionsAll.size());
+
+	}
+
+	/**
+	 * Case that simply adds a method invocation
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testDiffForTestJDT_1_0007d191() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		String fl = rootMegadiff.getAbsolutePath()
+				+ "/1_0007d191fec7fe2d6a0c4e87594cb286a553f92c/ASTInspector/1_0007d191fec7fe2d6a0c4e87594cb286a553f92c_ASTInspector_s.java";
+		String fr = rootMegadiff.getAbsolutePath()
+				+ "/1_0007d191fec7fe2d6a0c4e87594cb286a553f92c/ASTInspector/1_0007d191fec7fe2d6a0c4e87594cb286a553f92c_ASTInspector_t.java";
+		System.out.println(fl);
+
+		String lc = new String(Files.readAllBytes(new File(fl).toPath()));
+		Tree tl = new JdtTreeGenerator().generateFrom().string(lc).getRoot();
+
+		lc = new String(Files.readAllBytes(new File(fr).toPath()));
+		Tree tr = new JdtTreeGenerator().generateFrom().string(lc).getRoot();
+
+		TuningEngine engine = new TuningEngine();
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		ChawatheScriptGenerator edGenerator = new ChawatheScriptGenerator();
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+
+		EditScriptGenerator edGeneratorSimplified = new SimplifiedChawatheScriptGenerator();
+
+		System.out.println("Size  " + actionsAll.size());
+		System.out.println("All " + actionsAll);
+
+		assertEquals(886, actionsAll.size());
+
+		// Now simplified
+		actionsAll = engine.computeDiff(tl, tr, matcher, edGeneratorSimplified, new GumTreeProperties());
+		assertEquals(543, actionsAll.size());
+
+		System.out.println(actionsAll);
+
+		System.out.println("After configuring");
+		GumTreeProperties properies = new GumTreeProperties();
+		// vanillaDiffView_best_SimpleGumtree@GT_BUM_SMT_SBUP@0.1@GT_STM_MH@1.html
+		properies.getProperties().clear();
+		properies.put(ConfigurationOptions.GT_BUM_SMT_SBUP, 0.1);
+		properies.put(ConfigurationOptions.GT_STM_MH, 1);
+
+		actionsAll = engine.computeDiff(tl, tr, new CompositeMatchers.SimpleGumtree(), edGenerator, properies);
+
+		System.out.println(actionsAll.size());
+		System.out.println(actionsAll);
+
+		assertEquals(8, actionsAll.size());
+
+		List<Action> actionsSimplified = engine.computeDiff(tl, tr, new CompositeMatchers.SimpleGumtree(),
+				edGeneratorSimplified, properies);
+
+		assertEquals(1, actionsSimplified.size());
+
+	}
+
+	/**
+	 * Updates a comment
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testDiffForTestJDT_2_03() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		String fl = rootMegadiff.getAbsolutePath()
+				+ "/2_03b1dec4d20cee110b68cf8325f28f4403468317/FTPClient/2_03b1dec4d20cee110b68cf8325f28f4403468317_FTPClient_s.java";
+		String fr = rootMegadiff.getAbsolutePath()
+				+ "/2_03b1dec4d20cee110b68cf8325f28f4403468317/FTPClient/2_03b1dec4d20cee110b68cf8325f28f4403468317_FTPClient_t.java";
+		System.out.println(fl);
+
+		String lc = new String(Files.readAllBytes(new File(fl).toPath()));
+		Tree tl = new JdtTreeGenerator().generateFrom().string(lc).getRoot();
+
+		lc = new String(Files.readAllBytes(new File(fr).toPath()));
+		Tree tr = new JdtTreeGenerator().generateFrom().string(lc).getRoot();
+
+		TuningEngine engine = new TuningEngine();
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		ChawatheScriptGenerator edGenerator = new ChawatheScriptGenerator();
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+
+		EditScriptGenerator edGeneratorSimplified = new SimplifiedChawatheScriptGenerator();
+
+		System.out.println("Size  " + actionsAll.size());
+		System.out.println("All " + actionsAll);
+
+		assertTrue(actionsAll.size() > 100);
+
+		// Now simplified
+		actionsAll = engine.computeDiff(tl, tr, matcher, edGeneratorSimplified, new GumTreeProperties());
+		assertTrue(actionsAll.size() > 1);
+
+		System.out.println(actionsAll);
+
+		System.out.println("After configuring");
+
+		GumTreeProperties properies = new GumTreeProperties();
+		// vanillaDiffView_best_SimpleGumtree@GT_BUM_SMT_SBUP@0.1@GT_STM_MH@1.html
+		properies.getProperties().clear();
+		properies.put(ConfigurationOptions.GT_BUM_SMT_SBUP, 0.1);
+		properies.put(ConfigurationOptions.GT_STM_MH, 1);
+
+		actionsAll = engine.computeDiff(tl, tr, new CompositeMatchers.SimpleGumtree(), edGenerator, properies);
+
+		System.out.println(actionsAll.size());
+		System.out.println(actionsAll);
+
+		assertEquals(1, actionsAll.size());
+
+		List<Action> actionsSimplified = engine.computeDiff(tl, tr, new CompositeMatchers.SimpleGumtree(),
+				edGeneratorSimplified, properies);
+
+		assertEquals(1, actionsSimplified.size());
+
+	}
+
+	/**
+	 * Insertion of Import and update method invocation
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testDiffForTestJDT_3_04f() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		String fl = rootMegadiff.getAbsolutePath()
+				+ "/3_04f0e8f7a3545cf877c10967396b06595d57c34a/JavaExtensions/3_04f0e8f7a3545cf877c10967396b06595d57c34a_JavaExtensions_s.java";
+		String fr = rootMegadiff.getAbsolutePath()
+				+ "/3_04f0e8f7a3545cf877c10967396b06595d57c34a/JavaExtensions/3_04f0e8f7a3545cf877c10967396b06595d57c34a_JavaExtensions_t.java";
+		System.out.println(fl);
+
+		String lc = new String(Files.readAllBytes(new File(fl).toPath()));
+		Tree tl = new JdtTreeGenerator().generateFrom().string(lc).getRoot();
+
+		lc = new String(Files.readAllBytes(new File(fr).toPath()));
+		Tree tr = new JdtTreeGenerator().generateFrom().string(lc).getRoot();
+
+		TuningEngine engine = new TuningEngine();
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		ChawatheScriptGenerator edGenerator = new ChawatheScriptGenerator();
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+
+		EditScriptGenerator edGeneratorSimplified = new SimplifiedChawatheScriptGenerator();
+
+		System.out.println("Size  " + actionsAll.size());
+		System.out.println("All " + actionsAll);
+
+		// assertTrue(actionsAll.size() > 100);
+
+		// Now simplified
+		actionsAll = engine.computeDiff(tl, tr, matcher, edGeneratorSimplified, new GumTreeProperties());
+		assertTrue(actionsAll.size() > 2);
+
+		System.out.println(actionsAll);
+
+		System.out.println("After configuring");
+
+		GumTreeProperties properies = new GumTreeProperties();
+		// vanillaDiffView_best_SimpleGumtree@GT_BUM_SMT_SBUP@0.1@GT_STM_MH@1.html
+		properies.getProperties().clear();
+		properies.put(ConfigurationOptions.GT_BUM_SMT_SBUP, 0.1);
+		properies.put(ConfigurationOptions.GT_STM_MH, 1);
+
+		actionsAll = engine.computeDiff(tl, tr, new CompositeMatchers.SimpleGumtree(), edGenerator, properies);
+
+		System.out.println(actionsAll.size());
+		System.out.println(actionsAll);
+
+		// assertEquals(1, actionsAll.size());
+
+		List<Action> actionsSimplified = engine.computeDiff(tl, tr, new CompositeMatchers.SimpleGumtree(),
+				edGeneratorSimplified, properies);
+
+		assertEquals(3, actionsSimplified.size());
+
+	}
+
+	/**
+	 * Insertion of Import and update method invocation
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testDiffForTestJDT_java_example() throws Exception {
+
+		assertTrue(rootMegadiff.exists());
+
+		String fl = "./examples/java_simple_case_1/T1_s.java";
+		String fr = "./examples/java_simple_case_1/T1_s.java";
+		System.out.println(fl);
+
+		String lc = new String(Files.readAllBytes(new File(fl).toPath()));
+		Tree tl = new JdtTreeGenerator().generateFrom().string(lc).getRoot();
+
+		lc = new String(Files.readAllBytes(new File(fr).toPath()));
+		Tree tr = new JdtTreeGenerator().generateFrom().string(lc).getRoot();
+
+		TuningEngine engine = new TuningEngine();
+
+		CompositeMatchers.ClassicGumtree matcher = new CompositeMatchers.ClassicGumtree();
+		ChawatheScriptGenerator edGenerator = new ChawatheScriptGenerator();
+		List<Action> actionsAll = engine.computeDiff(tl, tr, matcher, edGenerator, new GumTreeProperties());
+
+		EditScriptGenerator edGeneratorSimplified = new SimplifiedChawatheScriptGenerator();
+
+		System.out.println("Size  " + actionsAll.size());
+		System.out.println("All " + actionsAll);
+
+		// assertTrue(actionsAll.size() > 100);
+
+		// Now simplified
+		actionsAll = engine.computeDiff(tl, tr, matcher, edGeneratorSimplified, new GumTreeProperties());
+		assertEquals(1, actionsAll.size());
+
+		System.out.println(actionsAll);
+
+		System.out.println("After configuring");
 
 	}
 
