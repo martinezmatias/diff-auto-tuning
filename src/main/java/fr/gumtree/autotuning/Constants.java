4928de578d0e20f805a2957ccb2a7b60fbf741c9
martinezmatias <matias.sebastian.martinez@gmail.com>
2021-03-08 19:27:12 +0100

    up

---
+++ src/main/java/fr/gumtree/autotuning/Constants.java
@@ -0,0 +1,32 @@
+package fr.gumtree.autotuning;
+
+public class Constants {
+	// TODO: CHange to enum
+	public static final String NR_TREEDELETE = "NR_TREEDELETE";
+	public static final String NR_TREEINSERT = "NR_TREEINSERT";
+	public static final String NR_MOVE = "NR_MOVE";
+	public static final String NR_UPDATE = "NR_UPDATE";
+	public static final String NR_DELETE = "NR_DELETE";
+	public static final String NR_INSERT = "NR_INSERT";
+	public static final String CONFIGS = "r";
+	public static final String MATCHER = "MATCHER";
+	public static final String TIME_MATCHER_ALL_CONFIGS = "TIME_MATCHER_ALL_CONFIGS";
+	public static final String MATCHERS = "MATCHERS";
+	public static final String TRIGHT = "TRIGHT";
+	public static final String TLEFT = "TLEFT";
+	public static final String CONFIG = "CONFIG";
+	public static final String TIME = "TIME";
+	public static final String NRROOTS = "NRROOTS";
+	public static final String NRACTIONS = "NRACTIONS";
+	public static final String STRUCTHASH = "STRUCTHASH";
+	public static final String HEIGHT = "HEIGHT";
+	public static final String SIZE = "SIZE";
+	public static final String MEGADIFFSET = "MEGADIFFSET";
+	public static final String COMMIT = "COMMIT";
+	public static final String FILE = "FILE";
+	public static final String TIMEOUT = "TIMEOUT";
+
+	// TIMES:
+	public static final String TIME_ALL_MATCHER_DIFF = "TIME_ALL_MATCHER_DIFF";
+	public static final String TIME_TREES_PARSING = "TIME_TREES_PARSING";
+}
